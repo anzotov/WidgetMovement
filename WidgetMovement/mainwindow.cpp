0952f1d45bd4535aaaffcfe151c7c42d599dfed4
Andrey Zotov <Andrey.N.Zotov@yandex.ru>
2024-03-24 15:30:00 +0500

    Refactor

--- WidgetMovement/mainwindow.cpp
+++ WidgetMovement/mainwindow.cpp
@@ -9,37 +9,42 @@ MainWindow::MainWindow(QWidget* parent) :
 {
     resize(300, 600);
     grabKeyboard();
+
     m_timer = new QTimer(this);
-    m_timer->start(1000);
     connect(m_timer, &QTimer::timeout, this, [this]() {
-                constexpr auto buttonSize = QSize(15, 15);
-                constexpr auto minCreateInvlMs = 100, maxInvlMs = 1000;
-                constexpr auto minMoveInvlMs = 15, maxMoveInvlMs = 30;
-                this->m_timer->setInterval(QRandomGenerator::global()->bounded(minCreateInvlMs, maxInvlMs));
+                this->m_timer->setInterval(QRandomGenerator::global()->bounded(100, 1000));
+
                 auto button = new QPushButton(this);
-                auto windowSize = this->size();
-                auto position = QPoint(QRandomGenerator::global()->bounded(0, windowSize.width() - buttonSize.width()), 100);
-                button->setGeometry(QRect(position, buttonSize));
+                constexpr auto buttonSize = QSize(15, 15);
+                auto buttonPos = QPoint(
+                    QRandomGenerator::global()->bounded(0, this->size().width() - buttonSize.width()), 
+                    100
+                );
+                button->setGeometry(QRect(buttonPos, buttonSize));
                 button->setText("*");
                 button->show();
+
                 auto buttonTimer = new QTimer(button);
-                auto moveInterval = QRandomGenerator::global()->bounded(minMoveInvlMs, maxMoveInvlMs);
-                buttonTimer->setInterval(moveInterval);
                 connect(buttonTimer, &QTimer::timeout, button, [button](){
                     auto movement = button->underMouse()?QPoint(0, 2):QPoint(0, 1);
                     button->move(button->pos()+movement);
+
                     if(!button->parentWidget()->contentsRect().contains(button->geometry())) {
                         button->deleteLater();
                     }
+
                     if(button->geometry().bottom() >= button->parentWidget()->contentsRect().bottom())
                     {
                         button->parentWidget()->setWindowTitle("Вы проиграли!");
                         button->parentWidget()->setStyleSheet("QMainWindow { background: red }");
                     }
                     }); 
+
                 connect(button, &QPushButton::pressed, button, [button](){
                     button->deleteLater();});
-                buttonTimer->start(); });
+
+                buttonTimer->start(QRandomGenerator::global()->bounded(15, 30)); });
+    m_timer->start(1000);
 }
 
 MainWindow::~MainWindow()
