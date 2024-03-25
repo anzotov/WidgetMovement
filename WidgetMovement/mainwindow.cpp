7af7a4208599b9f1bf2f0c1ab788e6e42f2bc6cc
Andrey Zotov <Andrey.N.Zotov@yandex.ru>
2024-03-25 20:21:55 +0500

    Устранение замечаний

--- WidgetMovement/mainwindow.cpp
+++ WidgetMovement/mainwindow.cpp
@@ -11,14 +11,15 @@ MainWindow::MainWindow(QWidget* parent) :
     grabKeyboard();
 
     m_timer = new QTimer(this);
-    connect(m_timer, &QTimer::timeout, this, [this]() {
-                this->m_timer->setInterval(QRandomGenerator::global()->bounded(100, 1000));
+    constexpr auto minCreationTimeMs = 100, maxCreationTimeMs = 1000;
+    connect(m_timer, &QTimer::timeout, this, [this, minCreationTimeMs, maxCreationTimeMs]() {
+                m_timer->setInterval(QRandomGenerator::global()->bounded(minCreationTimeMs, maxCreationTimeMs));
 
                 auto button = new QPushButton(this);
                 constexpr auto buttonSize = QSize(15, 15);
-                auto buttonPos = QPoint(
-                    QRandomGenerator::global()->bounded(0, this->size().width() - buttonSize.width()), 
-                    100
+                const auto buttonPos = QPoint(
+                    QRandomGenerator::global()->bounded(0, size().width() - buttonSize.width()), 
+                    QRandomGenerator::global()->bounded(0, 100 - buttonSize.height())
                 );
                 button->setGeometry(QRect(buttonPos, buttonSize));
                 button->setText("*");
@@ -44,7 +45,7 @@ MainWindow::MainWindow(QWidget* parent) :
                     button->deleteLater();});
 
                 buttonTimer->start(QRandomGenerator::global()->bounded(15, 30)); });
-    m_timer->start(1000);
+    m_timer->start(QRandomGenerator::global()->bounded(minCreationTimeMs, maxCreationTimeMs));
 }
 
 MainWindow::~MainWindow()
